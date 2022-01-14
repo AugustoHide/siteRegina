add228d41e0bbbcb384c5fadd03fddac4747362d
AugustoHide <augustohide@gmail.com>
2022-01-13 23:48:15 -0400

    término de template que vai ser apresentado pra regina

--- app.js
+++ app.js
@@ -16,6 +16,34 @@ app.get('/', (req, res) => {
     res.render('paginaInicial');
 });
 
+app.get('/terapia-tradicional', (req, res) => {
+    res.render('terapiaTradicional');
+});
+
+app.get('/terapia-alternativa', (req, res) => {
+    res.render('terapiaAlternativa');
+});
+
+app.get('/biomagnetismo', (req, res) => {
+    res.render('biomagnetismo');
+});
+
+app.get('/barras', (req, res) => {
+    res.render('barras');
+});
+
+app.get('/multidimensional', (req, res) => {
+    res.render('multidimensional');
+});
+
+app.get('/sobremim', (req, res) => {
+    res.render('sobremim');
+});
+
+app.get('/contatos', (req, res) => {
+    res.render('contatos');
+});
+
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
     console.log(`Serving on port ${port}!!!`);
