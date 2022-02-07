6d35863c40efbf07337d58f700a0322dc4857523
AugustoHide <augustohide@gmail.com>
2022-02-07 11:17:35 -0400

    SEO

--- app.js
+++ app.js
@@ -11,37 +11,47 @@ app.set('views', viewPath);
 
 app.use(express.static(path.join(__dirname, 'public')));
 
-
-app.get('/', (req, res) => {
-    res.render('paginaInicial');
+app.use((req, res, next) => { //Cria um middleware onde todas as requests passam por ele
+    if ((req.headers["x-forwarded-proto"] || "").endsWith("http")) //Checa se o protocolo informado nos headers é HTTP
+        res.redirect(`https://${req.hostname}${req.url}`); //Redireciona pra HTTPS
+    else //Se a requisição já é HTTPS
+        next(); //Não precisa redirecionar, passa para os próximos middlewares que servirão com o conteúdo desejado
 });
 
-app.get('/terapia-tradicional', (req, res) => {
-    res.render('terapiaTradicional');
+app.get('/', (req, res) => {
+    res.render('paginaInicial', { pagina: 'Home' });
 });
 
 app.get('/orientacao-familiar', (req, res) => {
-    res.render('orientacaoFamiliar');
+    res.render('orientacaoFamiliar', { pagina: 'Orientação Familiar' });
 });
 
 app.get('/terapia-casal', (req, res) => {
-    res.render('terapiaCasal');
+    res.render('terapiaCasal', { pagina: 'Terapia de Casal' });
 });
 
 app.get('/ansiedade-depressao', (req, res) => {
-    res.render('ansiedadeDepressao');
+    res.render('ansiedadeDepressao', { pagina: 'Ansiedade e Depressão' });
 });
 
 app.get('/positiva', (req, res) => {
-    res.render('positiva');
+    res.render('positiva', { pagina: 'Empoderamento' });
 });
 
 app.get('/sobremim', (req, res) => {
-    res.render('sobremim');
+    res.render('sobremim', { pagina: 'Sobre Mim' });
 });
 
 app.get('/contatos', (req, res) => {
-    res.render('contatos');
+    res.render('contatos', { pagina: 'Contatos' });
+});
+
+app.get('/robots.txt', (req, res) => {
+    res.sendFile('robots.txt');
+});
+
+app.get('/sitemap.xml', (req, res) => {
+    res.sendFile('sitemap.xml');
 });
 
 const port = process.env.PORT || 3000;
